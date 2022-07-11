ea920f538f374db5c57c0aeb8be47f347f4f4d3f
Ravi Narayan <ravi.narayan09@gmail.com>
2022-07-11 08:24:10 -0700

    5

---
+++ webappDD/mobile-chart.js
@@ -0,0 +1,35 @@
+const mobileCanvas = document.getElementById("mobile-chart");
+
+const mobileData = {
+    labels : ["Desktop", "Tablet", "Phone"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6c8',
+
+        ]
+    }]
+};
+
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
